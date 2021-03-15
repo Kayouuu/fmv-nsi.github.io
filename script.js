a071de7cac9db9f93d7e10206b075a40500582ad
Kayouuu <63807531+Kayouuu@users.noreply.github.com>
2021-03-15 14:34:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var vid = document.getElementById("myVideo"); 
+vid.addEventListener("ended", myHandler, false);
 
+function myHandler(e)
+{
+  console.log("aaaaa")
+}
 function playVid() { 
   vid.play(); 
 } 
