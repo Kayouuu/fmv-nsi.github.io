4f9491f6a12477c14920211ebff593eb9da6fefe
Kayouuu <saulnier254@gmail.com>
2021-04-08 19:05:56 +0200

    file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 var vid = document.getElementById("myVideo"); 
-vid.addEventListener("ended", myHandler, false);
+vid.addEventListener("ended", showBtn, false);
+
 var video = document.getElementsByTagName('video')[0];
 var sources = video.getElementsByTagName('source');
+var choix = document.getElementById("choix")
 var button1 = document.getElementById("button1")
+var button2 = document.getElementById("button2")
+var button3 = document.getElementById("button3")
+var button4 = document.getElementById("button4")
 
+var button_return = document.getElementById("return") 
 
-function myHandler(e)
+function showBtn(e)
 {
+  hideUnusedBtn()
   choix.style.opacity = 100;
 }
 
 function playVid() { 
   vid.play(); 
+  console.log("aaa")
 } 
 
 function pauseVid() { 
@@ -24,19 +32,70 @@ function YourFunctionName(){
 
 function changeButton(val)
 {
+  hideUnusedBtn()
   switch(val)
   {
-    case 1:
-      sources[0].src = 'vid/1.mp4';
+    case 0:
+      sources[0].src = 'vid/000.mp4';
       video.load();
-      button1.setAttribute('onClick','changeButton(4)')
-      button1.innerHTML = "dddd"
+    case 101:
+      sources[0].src = 'vid/101.mp4';
+      video.load();
+      button1.setAttribute('onClick','changeButton(4)') 
+      gameOver(000)
+      
       break;
-    case 2:
-      // a
-    case 4:
-      sources[0].src = 'vid/4.mp4'
+    case 102:
+      sources[0].src = 'vid/102.mp4';
+      video.load();
+      button1.setAttribute('onClick','changeButton(4)') 
+      hideBtn("")
+
+      break;
+    case 103:
+      sources[0].src = 'vid/103.mp4'
       video.load();
 
+    default:
+      console.log("erreur");
+  }
+}
+
+function hideBtn(val1, val2, val3 = "/", val4 = "/")
+{
+  choix.style.opacity = 0;
+
+  button1.innerHTML = val1
+  button2.innerHTML = val2
+  button3.innerHTML = val3
+  button4.innerHTML = val4
+
+  video.play();
+}
+function gameOver(val)
+{
+  video.play();
+  choix.style.opacity = 0;
+
+  button_return.style.opacity = 100;
+  button_return.setAttribute('onClick','changeButton( " '+val+' " )')
+}
+function hideUnusedBtn()
+{
+  if(button1.innerHTML = "//")
+  {
+    button1.style.opacity = 0;
+  }
+  if(button2.innerHTML = "//")
+  {
+    button2.style.opacity = 0;
+  }
+  if(button3.innerHTML = "//")
+  {
+    button3.style.opacity = 0;
+  }
+  if(button4.innerHTML = "//")
+  {
+    button4.style.opacity = 0;
   }
 }
